dd5cfe9545aceaed83ceef04aab8f191fd0d7c78
TexTrue <3140846162@qq.com>
2023-04-29 16:21:03 +0800

    fix

--- forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
+++ forge/src/main/java/city/windmill/ingameime/forge/mixin/MixinEditScreen.java
@@ -34,73 +34,78 @@ class MixinEditScreen {
     private void onRemove(CallbackInfo info) {
         IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditClose(this));
     }
+}
 
-    @Mixin(BookEditScreen.class)
-    abstract class MixinBookEditScreen {
-        @Inject(method = "convertLocalToScreen", at = @At("TAIL"))
-        private void onCaret_Book(Object pos2i, CallbackInfoReturnable<Object> cir) {
-            try {
-                Field pos2ix = pos2i.getClass().getDeclaredField("x");
-                Field pos2iy = pos2i.getClass().getDeclaredField("y");
-                pos2ix.setAccessible(true);
-                pos2iy.setAccessible(true);
-                IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) pos2ix.get(cir.getReturnValue()), (Integer) pos2iy.get(cir.getReturnValue()))));
-            } catch (Exception ignored) {
+@Mixin(BookEditScreen.class)
+abstract class MixinBookEditScreen {
+    @Inject(method = "convertLocalToScreen",
+            at = @At("TAIL"))
+    private void onCaret_Book(Object pos2i, CallbackInfoReturnable<Object> cir) {
+        try {
+            Field pos2ix = pos2i.getClass().getDeclaredField("x");
+            Field pos2iy = pos2i.getClass().getDeclaredField("y");
+            pos2ix.setAccessible(true);
+            pos2iy.setAccessible(true);
+            IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) pos2ix.get(cir.getReturnValue()), (Integer) pos2iy.get(cir.getReturnValue()))));
+        } catch (Exception ignored) {
 
-            }
-        }
-
-        @Inject(method = "render",
-                at = @At(value = "INVOKE",
-                        target = "Lnet/minecraft/client/gui/Font;draw(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/util/FormattedCharSequence;FFI)I"),
-                locals = LocalCapture.CAPTURE_FAILSOFT)
-        private void onCaret_Book(PoseStack poseStack, int i, int j, float f, CallbackInfo ci, int k, FormattedCharSequence formattedCharSequence, int m, int n) {
-            IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>(k + 36 + (114 + n) / 2 - Minecraft.getInstance().font.width("_"), 50)));
         }
     }
 
-    @Mixin(SignEditScreen.class)
-    abstract class MixinEditSignScreen extends Screen {
-
-        protected MixinEditSignScreen(Component p_i51108_1_) {
-            super(p_i51108_1_);
-        }
-
-        @Inject(method = "render",
-                at = {
-                        @At(value = "INVOKE",
-                                target = "Lnet/minecraft/client/gui/Font;drawInBatch(Ljava/lang/String;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZIIZ)I",
-                                ordinal = 1),
-                        @At(value = "INVOKE",
-                                target = "net/minecraft/client/gui/screens/inventory/SignEditScreen.fill(Lcom/mojang/blaze3d/vertex/PoseStack;IIIII)V",
-                                ordinal = 0)},
-                locals = LocalCapture.CAPTURE_FAILSOFT)
-        private void onCaret_Sign(PoseStack arg, int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f lv5, int p, String string, float q, int r, int s) {
-            //s(23)->x,o(17)->y
-            try {
-                Field m03 = lv5.getClass().getDeclaredField("m03");
-                Field m13 = lv5.getClass().getDeclaredField("m13");
-                m03.setAccessible(true);
-                m13.setAccessible(true);
-                IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) m03.get(lv5) + s, (Integer) m13.get(lv5) + o)));
-            } catch (Exception ignored) {
-
-            }
-        }
-
-        @Surrogate
-        private void onCaret_Sign(PoseStack arg, int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f lv5, int t, String string2, int u, int v) {
-            //v(22)->x,o(17)->y
-            try {
-                Field m03 = lv5.getClass().getDeclaredField("m03");
-                Field m13 = lv5.getClass().getDeclaredField("m13");
-                m03.setAccessible(true);
-                m13.setAccessible(true);
-                IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) m03.get(lv5) + v, (Integer) m13.get(lv5) + o)));
-            } catch (Exception ignored) {
-
-            }
-        }
+    @Inject(method = "render",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/Font;draw(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/util/FormattedCharSequence;FFI)I"),
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onCaret_Book(PoseStack poseStack, int i, int j, float f, CallbackInfo ci,
+                              int k, FormattedCharSequence formattedCharSequence, int m, int n) {
+        IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>(
+                k + 36 + (114 + n) / 2
+                        - Minecraft.getInstance().font.width("_"),
+                50
+        )));
     }
 }
 
+@Mixin(SignEditScreen.class)
+abstract class MixinEditSignScreen extends Screen {
+
+    protected MixinEditSignScreen(Component p_i51108_1_) {
+        super(p_i51108_1_);
+    }
+
+    @Inject(method = "render",
+            at = {
+                    @At(value = "INVOKE",
+                            target = "Lnet/minecraft/client/gui/Font;drawInBatch(Ljava/lang/String;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZIIZ)I",
+                            ordinal = 1),
+                    @At(value = "INVOKE",
+                            target = "net/minecraft/client/gui/screens/inventory/SignEditScreen.fill(Lcom/mojang/blaze3d/vertex/PoseStack;IIIII)V",
+                            ordinal = 0)},
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onCaret_Sign(PoseStack arg, int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f lv5, int p, String string, float q, int r, int s) {
+        //s(23)->x,o(17)->y
+        try {
+            Field m03 = lv5.getClass().getDeclaredField("m03");
+            Field m13 = lv5.getClass().getDeclaredField("m13");
+            m03.setAccessible(true);
+            m13.setAccessible(true);
+            IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) m03.get(lv5) + s, (Integer) m13.get(lv5) + o)));
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    @Surrogate
+    private void onCaret_Sign(PoseStack arg, int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f lv5, int t, String string2, int u, int v) {
+        //v(22)->x,o(17)->y
+        try {
+            Field m03 = lv5.getClass().getDeclaredField("m03");
+            Field m13 = lv5.getClass().getDeclaredField("m13");
+            m03.setAccessible(true);
+            m13.setAccessible(true);
+            IngameIMEForge.INSTANCE.getINGAMEIME_BUS().post(new ScreenEvents.EditCaret(this, new Pair<>((Integer) m03.get(lv5) + v, (Integer) m13.get(lv5) + o)));
+        } catch (Exception ignored) {
+
+        }
+    }
+}
