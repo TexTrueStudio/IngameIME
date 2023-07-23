23d7dee53261fe223aed809800b508b033b79d78
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-07-23 20:57:15 +0800

    1.18.2

--- common/src/main/kotlin/city/windmill/ingameime/client/gui/widget/AlphaModeWidget.kt
+++ common/src/main/kotlin/city/windmill/ingameime/client/gui/widget/AlphaModeWidget.kt
@@ -1,9 +1,9 @@
 package city.windmill.ingameime.client.gui.widget
 
 import city.windmill.ingameime.client.jni.ExternalBaseIME
+import com.mojang.blaze3d.vertex.PoseStack
 import kotlinx.coroutines.*
 import net.minecraft.client.gui.Font
-import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.resources.language.I18n
 import java.lang.ref.WeakReference
 
@@ -33,10 +33,10 @@ class AlphaModeWidget(font: Font) : Widget(font) {
         get() = 2 to 3
     
     @Suppress("NAME_SHADOWING")
-    override fun draw(guiGraphics: GuiGraphics, offsetX: Int, offsetY: Int, mouseX: Int, mouseY: Int, delta: Float) {
-        super.draw(guiGraphics, offsetX, offsetY, mouseX, mouseY, delta)
+    override fun draw(poseStack: PoseStack, offsetX: Int, offsetY: Int, mouseX: Int, mouseY: Int, delta: Float) {
+        super.draw(poseStack, offsetX, offsetY, mouseX, mouseY, delta)
         val offsetX = offsetX + width / 2 - font.width(text) / 2
         val offsetY = offsetY + padding.second
-        guiGraphics.drawString(font, text, offsetX, offsetY, textColor)
+        font.draw(poseStack, text, offsetX.toFloat(), offsetY.toFloat(), textColor)
     }
 }
